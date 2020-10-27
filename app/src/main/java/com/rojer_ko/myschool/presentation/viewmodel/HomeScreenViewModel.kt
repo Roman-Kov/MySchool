b3235abdd671fc36cc3b230cb2d0fce1ed32d6b5
Roman-Kov <rojer_ko@bk.ru>
2020-10-28 02:24:50 +0300

    Добавил viewModel для первого экрана

---
+++ app/src/main/java/com/rojer_ko/myschool/presentation/viewmodel/HomeScreenViewModel.kt
@@ -0,0 +1,82 @@
+package com.rojer_ko.myschool.presentation.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.rojer_ko.myschool.data.ISchoolRepository
+import com.rojer_ko.myschool.data.model.Homework
+import com.rojer_ko.myschool.data.model.SchoolClass
+import kotlinx.coroutines.*
+import java.time.LocalDate
+
+class HomeScreenViewModel(private val repository: ISchoolRepository): ViewModel() {
+
+    private val _nextExamLiveData = MutableLiveData<LocalDate>()
+    val nextExamLiveData: LiveData<LocalDate> = _nextExamLiveData
+
+    private val _classesLiveData = MutableLiveData<List<SchoolClass>>()
+    val classesLiveData: LiveData<List<SchoolClass>> = _classesLiveData
+
+    private val _homeWorksLiveData = MutableLiveData<List<Homework>>()
+    val homeWorksLiveData: LiveData<List<Homework>> = _homeWorksLiveData
+
+    private val nextExamCoroutineScope = CoroutineScope(
+        Dispatchers.IO
+                + SupervisorJob()
+                + CoroutineExceptionHandler { _, throwable ->
+            handleNextExamError(throwable)
+        })
+
+    private val classesCoroutineScope = CoroutineScope(
+        Dispatchers.IO
+                + SupervisorJob()
+                + CoroutineExceptionHandler { _, throwable ->
+            handleClassesError(throwable)
+        })
+
+    private val homeWorksCoroutineScope = CoroutineScope(
+        Dispatchers.IO
+                + SupervisorJob()
+                + CoroutineExceptionHandler { _, throwable ->
+            handleHomeWorksError(throwable)
+        })
+
+    private fun handleNextExamError(throwable: Throwable){
+        Log.e("Error", throwable.message?:"NextExam error")
+    }
+
+    private fun handleClassesError(throwable: Throwable){
+        Log.e("Error", throwable.message?:"Classes error")
+    }
+
+    private fun handleHomeWorksError(throwable: Throwable){
+        Log.e("Error", throwable.message?:"HomeWork error")
+    }
+
+    fun getClassesForDate(date: LocalDate){
+        classesCoroutineScope.launch {
+            _classesLiveData.postValue(repository.getClassesForDate(date))
+        }
+    }
+
+    fun getHomework(){
+        homeWorksCoroutineScope.launch {
+            _homeWorksLiveData.postValue(repository.getHomework())
+        }
+    }
+
+    fun getNextExamDate(){
+        nextExamCoroutineScope.launch {
+            _nextExamLiveData.postValue(repository.getNextExamDate())
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        nextExamCoroutineScope.coroutineContext.cancelChildren()
+        classesCoroutineScope.coroutineContext.cancelChildren()
+        homeWorksCoroutineScope.coroutineContext.cancelChildren()
+    }
+}
